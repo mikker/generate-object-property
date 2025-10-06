7f61d0d31de7a208d1a9a6f832d5a637961f1d36
Mikkel Malmberg <mikkel@brnbw.com>
2025-10-06 09:21:08 +0200

    Apply Prettier

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ gen.property = function (prop) {
 }
 
 gen.optional = function (obj, prop) {
-  return isProperty(prop) ? obj + '?.' + prop : obj + '?.[' + JSON.stringify(prop) + ']'
+  return isProperty(prop)
+    ? obj + '?.' + prop
+    : obj + '?.[' + JSON.stringify(prop) + ']'
 }
 
 module.exports = gen
 
-function gen (obj, prop) {
-  return isProperty(prop) ? obj + '.' + prop : obj + '[' + JSON.stringify(prop) + ']'
+function gen(obj, prop) {
+  return isProperty(prop)
+    ? obj + '.' + prop
+    : obj + '[' + JSON.stringify(prop) + ']'
 }
